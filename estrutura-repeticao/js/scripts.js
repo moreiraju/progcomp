3a6564715752880c0e300d843d6ef2ae6203f397
Júlia Moreira <evaleriamoreira@bol.com.br>
2025-05-05 19:28:44 -0300

    estrutura-repeticao

---
+++ estrutura-repeticao/js/scripts.js
@@ -0,0 +1,25 @@
+function exe0(){
+    let nota //guarda as notas
+    let conta = 1 // conta de 1 a 6
+    let soma = 0
+    while (conta <= 6){
+        nota = Number(prompt(`Informe a nota ${conta}`))
+        soma = soma + nota
+        conta++ //conta recebe conta +1
+    }
+    let media = soma/6
+    alert(`A média das notas é ${media.toFixed(1)}`)
+    
+    /*
+    let nota1 = Number(prompt('Informe a nota 1: '))
+    let nota2 = Number(prompt('Informe a nota 2: '))
+    let nota3 = Number(prompt('Informe a nota 3: '))
+    let nota4 = Number(prompt('Informe a nota 4: '))
+    let nota5 = Number(prompt('Informe a nota 5: '))
+    let nota6 = Number(prompt('Informe a nota 6: '))
+
+    let soma = nota1 + nota2 + nota3 + nota4 + nota5 + nota6
+    let media = soma/6
+
+    // Mostra o resultado*/
+}
